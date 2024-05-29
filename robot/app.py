48d983ee323aeafc109cb5d74387bbc95f02ce14
iBott <61185052+ecrespo66@users.noreply.github.com>
2024-05-29 12:44:21 +0200

    Update app.py

--- robot/app.py
+++ robot/app.py
@@ -19,16 +19,13 @@ class App:
 
     @classmethod
     def load_certificate(self, browser):
-        time.sleep(10)
+        time.sleep(30)
         # Crea un objeto Desktop para interactuar con la interfaz de usuario de Windows
         desktop = Desktop(backend="uia")
         main_window = desktop.window(title="Seleccionar un certificado", top_level_only=False, found_index=0)
         main_window.wait('visible')
         main_window.set_focus()
         main_window.child_window(title="Aceptar", control_type="Button").click()
-        actions = ActionChains(browser)
-        actions.send_keys(Keys.ENTER)
-        actions.perform()
 
 
     def login(self):

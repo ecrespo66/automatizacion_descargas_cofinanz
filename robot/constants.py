708501432555dbe327d4eb9830fd9f4019e5f3b0
ecrespo <enrique.crespo.debenito@gmail.com>
2024-06-24 10:28:36 +0200

    first version

--- robot/constants.py
+++ robot/constants.py
@@ -7,4 +7,4 @@
 TEMP_FOLDER = "C:\\Robot\\Downloads\\CONTABLEFISCAL\\TEMP"
 INPUT_FILE = "C:\\Users\\Administrador\\Documents\\listado clientes.xlsx"
 DOWNLOAD_FOLDER = "C:\\Robot\\Downloads\\CONTABLEFISCAL\\CLIENTES"
-COMMON_FOLDER = "C:\\Robot\\Downloads\\COMUNES\\IMPUESTOS DIAMAWEB\\"
+COMMON_FOLDER = "M:\\COMUNES\\IMPUESTOS DIAMAWEB\\"
